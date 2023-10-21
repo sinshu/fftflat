023c5b1412ee21023e2d33dfa9372bcc0f0ba1e1
Nobuaki Tanaka <sinshu.d@gmail.com>
2023-10-21 15:44:38 +0900

    Add more tests.

--- FftFlatTest/RoundTrip.cs
+++ FftFlatTest/RoundTrip.cs
@@ -14,13 +14,42 @@ namespace FftFlatTest
         [TestCase(256)]
         [TestCase(512)]
         [TestCase(1024)]
-        public void Random(int length)
+        public void Random1(int length)
         {
             var random = new Random(42);
             var expected = new Complex[length];
             for (var i = 0; i < length; i++)
             {
                 expected[i] = new Complex(random.NextDouble(), random.NextDouble());
+            }
+
+            var actual = expected.ToArray();
+            var fft = new FftFlat.Fft(length);
+            fft.ForwardInplace(actual);
+            fft.InverseInplace(actual);
+
+            for (var i = 0; i < length; i++)
+            {
+                Assert.That(actual[i].Real, Is.EqualTo(expected[i].Real).Within(1.0E-6));
+                Assert.That(actual[i].Imaginary, Is.EqualTo(expected[i].Imaginary).Within(1.0E-6));
+            }
+        }
+
+        [TestCase(8)]
+        [TestCase(16)]
+        [TestCase(32)]
+        [TestCase(64)]
+        [TestCase(128)]
+        [TestCase(256)]
+        [TestCase(512)]
+        [TestCase(1024)]
+        public void Random2(int length)
+        {
+            var random = new Random(42);
+            var expected = new Complex[length];
+            for (var i = 0; i < length; i++)
+            {
+                expected[i] = new Complex(random.NextDouble() - 0.5, random.NextDouble() - 0.5);
             }
 
             var actual = expected.ToArray();
