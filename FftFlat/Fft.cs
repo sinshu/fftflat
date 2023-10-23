9fb6d02d34ec3202587297fa18ecc86474c0c3ae
Nobuaki Tanaka <sinshu.d@gmail.com>
2023-10-23 22:16:49 +0900

    Some changes.

--- FftFlat/Fft.cs
+++ FftFlat/Fft.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Buffers;
 using System.Numerics;
+using System.Runtime.InteropServices;
 
 namespace FftFlat
 {
@@ -10,6 +12,7 @@ namespace FftFlat
         private readonly Complex[] twiddlesInverse;
         private readonly int[] stageRadix;
         private readonly int[] stageRemainder;
+        private readonly double inverseScaling;
 
         public Fft(int length)
         {
@@ -55,129 +58,140 @@ namespace FftFlat
             while (n > 1);
             stageRadix = stageRadixList.ToArray();
             stageRemainder = stageRemainderList.ToArray();
+
+            inverseScaling = 1.0 / length;
         }
 
         public void ForwardInplace(Span<Complex> values)
         {
-            var fft_out = new Complex[length];
-            transform(values, fft_out, 0, 0, false, 0, 1);
-            fft_out.CopyTo(values);
+            var result = ArrayPool<Complex>.Shared.Rent(length);
+            try
+            {
+                Transform(values, result, 0, 0, false, 0, 1);
+                result.AsSpan(0, length).CopyTo(values);
+            }
+            finally
+            {
+                ArrayPool<Complex>.Shared.Return(result);
+            }
         }
 
         public void InverseInplace(Span<Complex> values)
         {
-            var fft_out = new Complex[length];
-            transform(values, fft_out, 0, 0, true, 0, 1);
-            for (var i = 0; i < values.Length; i++)
+            var result = ArrayPool<Complex>.Shared.Rent(length);
+            try
             {
-                values[i] = fft_out[i] / length;
+                Transform(values, result, 0, 0, true, 0, 1);
+
+                var vv = MemoryMarshal.Cast<Complex, Vector<double>>(values);
+                var vr = MemoryMarshal.Cast<Complex, Vector<double>>(result);
+                var count = 0;
+                for (var i = 0; i < vv.Length; i++)
+                {
+                    vv[i] = vr[i] * inverseScaling;
+                    count += Vector<double>.Count;
+                }
+                for (var i = count; i < values.Length; i++)
+                {
+                    values[i] = result[i] * inverseScaling;
+                }
+            }
+            finally
+            {
+                ArrayPool<Complex>.Shared.Return(result);
             }
         }
 
-        public void Forward(ReadOnlySpan<Complex> fft_in, Span<Complex> fft_out)
+        public void Forward(ReadOnlySpan<Complex> source, Span<Complex> destination)
         {
-            transform(fft_in, fft_out, 0, 0, false, 0, 1);
+            Transform(source, destination, 0, 0, false, 0, 1);
         }
 
-        private void transform(ReadOnlySpan<Complex> source, Span<Complex> destination, int fft_in, int fft_out, bool inverse, int stage = 0, int fstride = 1)
+        private void Transform(ReadOnlySpan<Complex> src, Span<Complex> dst, int srcStart, int dstStart, bool inverse, int stage, int stride)
         {
             var p = stageRadix[stage];
             var m = stageRemainder[stage];
-            int Fout_beg = fft_out;
-            int Fout_end = fft_out + p * m;
+
+            var dstCount = p * m;
+            var dstIndex = dstStart;
+            var dstEnd = dstStart + dstCount;
 
             if (m == 1)
             {
                 do
                 {
-                    destination[fft_out] = source[fft_in];
-                    fft_in += fstride;
+                    dst[dstIndex] = src[srcStart];
+                    srcStart += stride;
+                    dstIndex++;
                 }
-                while (++fft_out != Fout_end);
+                while (dstIndex != dstEnd);
             }
             else
             {
                 do
                 {
-                    // recursive call:
-                    // DFT of size m*p performed by doing
-                    // p instances of smaller DFTs of size m,
-                    // each one takes a decimated version of the input
-                    transform(source, destination, fft_in, fft_out, inverse, stage + 1, fstride * p);
-                    fft_in += fstride;
+                    Transform(src, dst, srcStart, dstIndex, inverse, stage + 1, stride * p);
+                    srcStart += stride;
+                    dstIndex += m;
                 }
-                while ((fft_out += m) != Fout_end);
+                while (dstIndex != dstEnd);
             }
 
-            fft_out = Fout_beg;
+            var dstSlice = dst.Slice(dstStart, dstCount);
+            var twiddles = inverse ? twiddlesInverse : twiddlesForward;
 
-            var dst = destination.Slice(fft_out, Fout_end - Fout_beg);
-
-            // recombine the p smaller DFTs
             switch (p)
             {
-                case 2: kf_bfly2(dst, inverse, fstride, m); break;
-                case 3: kf_bfly3(dst, inverse, fstride, m); break;
-                case 4: kf_bfly4(dst, inverse, fstride, m); break;
-                case 5: kf_bfly5(dst, inverse, fstride, m); break;
-                default: kf_bfly_generic(dst, inverse, fstride, m, p); break;
+                case 2:
+                    Sub2(twiddles, dstSlice, inverse, stride, m);
+                    break;
+                case 4:
+                    Sub4(twiddles, dstSlice, inverse, stride, m);
+                    break;
+                default:
+                    throw new NotImplementedException("Arbitrary length FFT is not yet implemented.");
             }
         }
 
-        private void kf_bfly2(Span<Complex> Fout, bool inverse, int fstride, int m)
+        private static void Sub2(Span<Complex> twiddles, Span<Complex> dst, bool inverse, int stride, int count)
         {
-            var twiddles = inverse ? twiddlesInverse : twiddlesForward;
-
-            for (var k = 0; k < m; ++k)
+            for (var i = 0; i < count; i++)
             {
-                var t = Fout[m + k] * twiddles[k * fstride];
-                Fout[m + k] = Fout[k] - t;
-                Fout[k] += t;
+                var t = dst[count + i] * twiddles[i * stride];
+                dst[count + i] = dst[i] - t;
+                dst[i] += t;
             }
         }
 
-        private void kf_bfly4(Span<Complex> Fout, bool inverse, int fstride, int m)
+        private static void Sub4(Span<Complex> twiddles, Span<Complex> dst, bool inverse, int stride, int m)
         {
-            var twiddles = inverse ? twiddlesInverse : twiddlesForward;
-
             Span<Complex> scratch = stackalloc Complex[7];
-            var negative_if_inverse = inverse ? -1 : +1;
 
-            for (var k = 0; k < m; ++k)
+            for (var i = 0; i < m; i++)
             {
-                scratch[0] = Fout[k + m] * twiddles[k * fstride];
-                scratch[1] = Fout[k + 2 * m] * twiddles[k * fstride * 2];
-                scratch[2] = Fout[k + 3 * m] * twiddles[k * fstride * 3];
-                scratch[5] = Fout[k] - scratch[1];
+                scratch[0] = dst[i + m] * twiddles[i * stride];
+                scratch[1] = dst[i + 2 * m] * twiddles[i * stride * 2];
+                scratch[2] = dst[i + 3 * m] * twiddles[i * stride * 3];
+                scratch[5] = dst[i] - scratch[1];
 
-                Fout[k] += scratch[1];
+                dst[i] += scratch[1];
                 scratch[3] = scratch[0] + scratch[2];
                 scratch[4] = scratch[0] - scratch[2];
-                scratch[4] = new(scratch[4].Imaginary * negative_if_inverse,
-                                      -scratch[4].Real * negative_if_inverse);
+                if (inverse)
+                {
+                    scratch[4] = new(-scratch[4].Imaginary, scratch[4].Real);
+                }
+                else
+                {
+                    scratch[4] = new(scratch[4].Imaginary, -scratch[4].Real);
+                }
 
-                Fout[k + 2 * m] = Fout[k] - scratch[3];
-                Fout[k] += scratch[3];
-                Fout[k + m] = scratch[5] + scratch[4];
-                Fout[k + 3 * m] = scratch[5] - scratch[4];
+                dst[i + 2 * m] = dst[i] - scratch[3];
+                dst[i] += scratch[3];
+                dst[i + m] = scratch[5] + scratch[4];
+                dst[i + 3 * m] = scratch[5] - scratch[4];
             }
         }
-
-        private void kf_bfly3(Span<Complex> Fout, bool inverse, int fstride, int m)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void kf_bfly5(Span<Complex> Fout, bool inverse, int fstride, int m)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void kf_bfly_generic(Span<Complex> Fout, bool inverse, int fstride, int m, int p)
-        {
-            throw new NotImplementedException();
-        }
-
 
         public int Length => length;
 
