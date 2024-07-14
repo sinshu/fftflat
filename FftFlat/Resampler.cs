0708a7bf92f6f2748ba0260f25382b7d72ed8de1
Nobuaki Tanaka <sinshu.d@gmail.com>
2024-07-14 16:52:11 +0900

    Add sinc function.

---
+++ FftFlat/Resampler.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+
+namespace FftFlat
+{
+    public sealed class Resampler
+    {
+        public void Resample(ReadOnlySpan<double> source, ReadOnlySpan<double> detsination)
+        {
+            using (var writer = new StreamWriter("test.csv"))
+            {
+                for (var i = -1000; i <= 1000; i++)
+                {
+                    var x = (double)i / 100;
+                    writer.WriteLine(Sinc(x));
+                }
+            }
+        }
+
+        internal static double Sinc(double x)
+        {
+            var y = Math.PI * x;
+            if (Math.Abs(y) < 1.0E-15)
+            {
+                return 1.0;
+            }
+            else
+            {
+                return Math.Sin(y) / y;
+            }
+        }
+    }
+}
---
+++ FftFlatTest/ResamplerTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+
+namespace FftFlatTest
+{
+    public class ResamplerTest
+    {
+        [Test]
+        public void Sinc()
+        {
+            for (var x = -10; x <= 10; x++)
+            {
+                if (x == 0)
+                {
+                    Assert.That(FftFlat.Resampler.Sinc(x), Is.EqualTo(1.0));
+                }
+                else
+                {
+                    Assert.That(FftFlat.Resampler.Sinc(x), Is.EqualTo(0.0).Within(1.0E-12));
+                }
+            }
+
+            for (var i = 0; i <= 10; i++)
+            {
+                var x1 = i + 0.5;
+                var x2 = i + 1.5;
+                var y1 = FftFlat.Resampler.Sinc(x1);
+                var y2 = FftFlat.Resampler.Sinc(x2);
+                Assert.That(Math.Sign(y1) == -Math.Sign(y2));
+                Assert.That(Math.Abs(y1) > Math.Abs(y2));
+                Assert.That(Math.Abs(y2) < 0.22);
+                Assert.That(Math.Abs(y2) > 0.0027);
+            }
+
+            for (var i = 0; i <= 10; i++)
+            {
+                var x1 = -i - 0.5;
+                var x2 = -i - 1.5;
+                var y1 = FftFlat.Resampler.Sinc(x1);
+                var y2 = FftFlat.Resampler.Sinc(x2);
+                Assert.That(Math.Sign(y1) == -Math.Sign(y2));
+                Assert.That(Math.Abs(y1) > Math.Abs(y2));
+                Assert.That(Math.Abs(y2) < 0.22);
+                Assert.That(Math.Abs(y2) > 0.0027);
+            }
+        }
+    }
+}
